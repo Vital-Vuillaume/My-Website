d23e1869eeb37a5c70a20f0f2a8cf23073f0da1b
Vital Vuillaume <vitalvuillaume@proton.me>
2023-07-18 17:35:38 +0200

    Add files via upload

--- Project-Booklet.js
+++ Project-Booklet.js
@@ -1,11 +1,16 @@
-//changement de page
-
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const section1 = document.getElementById('section1');
 const section2 = document.getElementById('section2');
 const section3 = document.getElementById('section3');
+const btn = document.querySelector(".btn");
+const nav = document.querySelector(".nav");
+const image1 = document.querySelector('#btn1 .img');
+const image2 = document.querySelector('#btn2 .img');
+const image3 = document.querySelector('#btn3 .img');
+
+//-----Changement de page-----\\
 
 function showSection(section) {
   section1.style.display = 'none';
@@ -26,15 +31,61 @@ btn3.addEventListener('click', function() {
   showSection(section3);
 });
 
-// Afficher la section 1 par défaut lors du chargement initial de la page
+//-----Afficher la section 1 par défaut----\\
 
 window.addEventListener('load', function() {
   showSection(section1);
 });
 
-const btn = document.querySelector(".btn");
-const nav = document.querySelector(".nav");
+//-----Barre de navigation qui s'agrandit-----\\
 
 btn.onclick = function ajouterClassNav() {
    nav.classList.toggle("active"); 
+};
+
+//-----Afficher Accueil par défaut-----\\
+
+btn1.classList.add('active');
+image1.src = 'accueil-active.png';
+
+//-----changement des boutons-----\\
+
+btn1.onclick = function() {
+  if (btn1.classList.contains('active')) {
+    return;
+  }
+
+  image1.src = 'accueil-active.png';
+  btn1.classList.add('active');
+  image2.src = 'recherche.png';
+  btn2.classList.remove('active');
+  image3.src = 'parametres.png';
+  btn3.classList.remove('active');
+};
+
+btn2.onclick = function() {
+  if (btn2.classList.contains('active')) {
+    return;
+  }
+
+  image2.src = 'recherche-active.png';
+  btn2.classList.add('active');
+  image1.src = 'accueil.png';
+  btn1.classList.remove('active');
+  image3.src = 'parametres.png';
+  btn3.classList.remove('active');
+};
+
+
+btn3.onclick = function() {
+  if (btn3.classList.contains('active')) {
+    return;
+  }
+
+  image3.src = 'parametres-active.png';
+  btn3.classList.add('active');
+  image1.src = 'accueil.png';
+  btn1.classList.remove('active');
+  image2.src = 'recherche.png';
+  btn2.classList.remove('active');
 };
